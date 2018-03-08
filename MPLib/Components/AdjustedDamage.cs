52f4a8c6ab7b417830d2dea9117e060cf401df4c
DW01 <dark@zejroleplaying.org>
2018-03-08 16:53:16 -0500

    Initial commit

---
+++ MPLib/Components/AdjustedDamage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPLib.Components
+{
+    // Adjusted damage calculations.
+    // We'll have various methods here to account for Piercing, etc..
+    class AdjustedDamage
+    {
+        // Damage type enumerator.
+        public enum DamageTypes
+        {
+            Normal,
+            Piercing,
+            Destruct
+        }
+
+        // Damage types reference.
+        public DamageTypes DamageType;
+
+        // Intermediate output.
+        float IntermediateOutput;
+
+        // Final output variable.
+        float FinalOutput;
+
+        // Basic damage calculation method.
+        // Uses multipliers instead of percentages for more readable calcs.
+        public float CalculateAdjustedDamage(float DamageMult)
+        {
+            // Is damage type normal?
+            if (DamageType == DamageTypes.Normal)
+            {
+                // Damage multiplier below 2.0.
+                if (DamageMult < 2.0f)
+                {
+                    IntermediateOutput = (10 * DamageMult) - 10;
+                }
+
+                // Damage multiplier 2.0 or above.
+                else
+                {
+                    IntermediateOutput = (20 * DamageMult) - 28;
+                }
+                FinalOutput = IntermediateOutput;
+            }
+
+            // Is damage type piercing?
+            else if (DamageType == DamageTypes.Piercing)
+            {
+                // Damage Calcs.
+                IntermediateOutput = ((40 * DamageMult) - 4) / 3;
+                FinalOutput = IntermediateOutput;
+            }
+
+            // Is damage type destruct?
+            else if (DamageType == DamageTypes.Destruct)
+            {
+                // Used in 90% of cases.
+                if (IntermediateOutput > 0.0f)
+                {
+                    FinalOutput = IntermediateOutput * 1.5f;
+                }
+
+                // We should rarely see this branch,
+                // but if we do, prevent negative mult
+                // by simply adding 1 to the value.
+                else
+                {
+                    FinalOutput = IntermediateOutput + 1;
+                }
+            }
+
+            // Return output.
+            return FinalOutput;
+        }
+    }
+}
---
+++ MPLib/Components/ValueCaps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPLib.Components
+{
+    class ValueCaps
+    {
+        // Universally speaking, this should default to 4.
+        // However, there will be cases where we'll need to pass in another value.
+        // So we declare MinValueCap here and leave it as-is.
+        int MinValue;
+        int MinValueCap;
+
+        // Minimum MP value soft cap.
+        // Usually we'll use this instead of MinValue directly.
+        public int MinValueSoftCap(int PreCalc, int PostCalc)
+        {
+            MinValueCap = Math.Max(MinValue, PreCalc) + PostCalc;
+            return MinValueCap;
+        }
+    }
+}
