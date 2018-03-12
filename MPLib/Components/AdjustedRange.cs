4ca864edad9755fa755c5c6a7a997ef276f23b1c
DW01 <dark@zejroleplaying.org>
2018-03-12 07:09:35 -0400

    More updates
    - Added some methods to calculate Adjusted Range. Very WIP.
    
    - Actually filled out Assembly Info for MPLib.

--- MPLib/Components/AdjustedRange.cs
+++ MPLib/Components/AdjustedRange.cs
@@ -11,6 +11,48 @@ namespace MPLib.Components
     // spit out MP cost contributor.
     class AdjustedRange
     {
-        
+        #region GLOBALS
+        // Final range output.
+        public float FinalRange;
+        #endregion
+
+        #region BASIC_SINGLE_SOURCE
+        // Basic Adjusted Range calculatio method.
+        // Uses Dmax - 1 to calculate range.
+        public float CalculateBasicAdjustedRange(int MaximumDistance)
+        {
+            FinalRange = MaximumDistance - 1;
+            return FinalRange;
+        }
+        #endregion
+
+        #region COMPLEX_SINGLE_SOURCE
+        // More complete single-source Adjusted Range calculation.
+        public float CalculateSingleSourceAdjustedRange(int MaximumDistance, int NullDistance)
+        {
+            FinalRange = MaximumDistance - (NullDistance + 1);
+            return FinalRange;
+        }
+        #endregion
+
+        #region BASIC_MULTI_SOURCE
+        // Calculate basic Adjusted Range for multiple sources.
+        public float CalculateBasicMultiSourceAdjustedRange(int MaximumDistance, int[] Sources)
+        {
+            // Compunded source variables.
+            float Source;
+            float AllSources;
+
+            // Main loop.
+            foreach (int CurrentSource in Sources)
+            {
+                Source = CalculateBasicAdjustedRange(MaximumDistance);
+                AllSources = Source++;
+                FinalRange = AllSources;
+            }
+
+            return FinalRange;
+        }
+        #endregion
     }
 }
--- MPLib/Properties/AssemblyInfo.cs
+++ MPLib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MPLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ZEJ Roleplaying MP Coster Core Library")]
+[assembly: AssemblyDescription("Core functions of the graphical MP coster.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MPLib")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Nebulon Ranger")]
+[assembly: AssemblyProduct("ZEJ Roleplaying MP Coster")]
+[assembly: AssemblyCopyright("Copyright © Nebulon Ranger 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
